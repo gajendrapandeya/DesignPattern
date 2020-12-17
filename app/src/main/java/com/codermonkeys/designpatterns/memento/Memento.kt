5d4b8455a3c669f7fc39d4181aebf1bf0e7e2ffe
jajendrapandeya <=>
2020-12-17 14:41:19 +0545

    #Momento Pattern

---
+++ app/src/main/java/com/codermonkeys/designpatterns/memento/Memento.kt
@@ -0,0 +1,44 @@
+package com.codermonkeys.designpatterns.memento
+
+fun main() {
+
+    val editor = Editor("a")
+    val history = History()
+
+    editor.content = "a"
+    history.push(editor.createState())
+
+    editor.content = "b"
+    history.push(editor.createState())
+
+    editor.content = "c"
+    editor.restore(history.pop())
+    editor.restore(history.pop())
+
+    println(editor.content)
+
+}
+
+class Editor(var content: String) {
+    
+    fun createState(): EditorState = EditorState(content)
+    fun restore(state: EditorState) {
+        content = state.content
+    }
+}
+
+class EditorState(val content: String)
+
+class History {
+
+    private val states = ArrayList<EditorState>()
+
+    fun push(state: EditorState) = states.add(state)
+
+    fun pop(): EditorState {
+        val lastIndex = states.size - 1
+        val lastState = states[lastIndex]
+        states.remove(lastState)
+        return lastState
+    }
+}
